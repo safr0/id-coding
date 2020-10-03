32bcd882e16439e5e66e75ff72b4a9a8b2d5a655
Muhammad Amir <sheikh.amir@gmaill.com>
2020-10-03 14:43:35 +1000

    Unit tests project setup

---
+++ Logic.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Presentation.Controllers;
+
+namespace Logic.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual(".id", result.ViewBag.Message);
+        }
+    }
+}
